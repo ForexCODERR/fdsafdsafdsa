539f786fc4782aa14d3355cd0d8e82102fbae0b6
ForexCODERR <147941246+ForexCODERR@users.noreply.github.com>
2023-11-01 07:30:10 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,15 @@
+local gameId = game.GameId
+
+if gameId == 8204899140 or gameId == 8206123457 or gameId == 8206126987 then
+
+    loadstring(game:HttpGet("https://github.com/ForexCODERR/fdsafdsafdsa/blob/main/f2f1321.lua"))()
+    print("Vision's Football Fusion 2 Has Loaded!")
+
+elseif gameId == 4777817887 then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ForexCODERR/fdsafdsafdsa/main/fdsahfdsahfdshafhdsa.lua"))()
+    print("Vision's BladeBall Has Loaded!")
+
+else
+    game.Players.LocalPlayer:Kick("Unsupported")
+end
