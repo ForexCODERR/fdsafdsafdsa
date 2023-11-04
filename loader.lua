a823473a578646261e6bc8415395a55ae322b5a5
ForexCODERR <147941246+ForexCODERR@users.noreply.github.com>
2023-11-03 19:03:53 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ local gameId = game.GameId
 
 if gameId == 8204899140 or gameId == 8206123457 or gameId == 8206126987 then
 
-    loadstring(game:HttpGet("https://github.com/ForexCODERR/fdsafdsafdsa/blob/main/f2f1321.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ForexCODERR/fdsafdsafdsa/main/ff2.lua"))()
     print("Vision's Football Fusion 2 Has Loaded!")
 
 elseif gameId == 4777817887 then
